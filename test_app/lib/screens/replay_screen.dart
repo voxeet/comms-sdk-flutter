13eb66b3e25fc568f8bdb3072b2414c6f5a59b2a
Gabriel Radu <gabriel.radu@dolby.com>
2022-09-25 13:27:28 +0200

    Create close button for conference replay screen

--- test_app/lib/screens/replay_screen.dart
+++ test_app/lib/screens/replay_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:dolbyio_comms_sdk_flutter/dolbyio_comms_sdk_flutter.dart';
+import '/widgets/bottom_tool_bar.dart';
+import '/widgets/conference_action_icon_button.dart';
 import '/widgets/dolby_title.dart';
 import 'participant_screen/participant_grid.dart';
 
@@ -23,11 +25,11 @@ class _ReplayScreenState extends State<ReplayScreen> {
           constraints: const BoxConstraints.expand(),
           decoration: const BoxDecoration(color: Colors.deepPurple),
           child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const DolbyTitle(title: 'Dolby.io', subtitle: 'Flutter SDK'),
-                ReplayScreenContent(conference: widget.conference)
-              ]
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const DolbyTitle(title: 'Dolby.io', subtitle: 'Flutter SDK'),
+              ReplayScreenContent(conference: widget.conference)
+            ],
           ),
         ),
       ),
@@ -37,32 +39,38 @@ class _ReplayScreenState extends State<ReplayScreen> {
 
 class ReplayScreenContent extends StatefulWidget {
   final Conference conference;
-  const ReplayScreenContent({Key? key, required this.conference}) : super(key: key);
+  const ReplayScreenContent({Key? key, required this.conference})
+      : super(key: key);
 
   @override
   State<ReplayScreenContent> createState() => _ReplayScreenContentState();
 }
 
 class _ReplayScreenContentState extends State<ReplayScreenContent> {
-
   final _dolbyioCommsSdkFlutterPlugin = DolbyioCommsSdk.instance;
-  StreamSubscription<Event<ConferenceServiceEventNames, ConferenceStatus>>? conferenceStatusChangeSubscription;
+  StreamSubscription<Event<ConferenceServiceEventNames, ConferenceStatus>>?
+      conferenceStatusChangeSubscription;
   List<Participant> participants = [];
 
   @override
   void initState() {
     super.initState();
-    conferenceStatusChangeSubscription =
-        _dolbyioCommsSdkFlutterPlugin.conference.onStatusChange().listen((event) {
-          if(event.body.encode() == ConferenceStatus.ended.encode()) {
-            Navigator.of(context).popUntil(ModalRoute.withName("JoinConferenceScreen"));
-          }
-        });
+    conferenceStatusChangeSubscription = _dolbyioCommsSdkFlutterPlugin
+        .conference
+        .onStatusChange()
+        .listen((event) {
+      if (event.body.encode() == ConferenceStatus.ended.encode()) {
+        Navigator.of(context)
+            .popUntil(ModalRoute.withName("JoinConferenceScreen"));
+      }
+    });
   }
 
   @override
   void deactivate() {
     conferenceStatusChangeSubscription?.cancel();
+    var options = ConferenceLeaveOptions(false);
+    _dolbyioCommsSdkFlutterPlugin.conference.leave(options: options);
     super.deactivate();
   }
 
@@ -72,28 +80,35 @@ class _ReplayScreenContentState extends State<ReplayScreenContent> {
       child: Container(
         decoration: const BoxDecoration(
             color: Colors.white,
-            borderRadius: BorderRadius.vertical(top: Radius.circular(12))
-        ),
+            borderRadius: BorderRadius.vertical(top: Radius.circular(12))),
         child: Column(
           children: [
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 16),
               child: Center(
-                  child: Text(
-                      widget.conference.id!,
-                      style: const TextStyle(fontSize: 16)),
+                child: Text(widget.conference.id!,
+                    style: const TextStyle(fontSize: 16)),
               ),
             ),
-            Expanded(
-              child: Stack(
-                children: const [
-                  ParticipantGrid(),
-                ],
-              ),
-            ),
+            const Expanded(child: ParticipantGrid()),
+            const ReplayControllsWidget(),
           ],
         ),
       ),
     );
+  }
+}
+
+class ReplayControllsWidget extends StatelessWidget {
+  const ReplayControllsWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomToolBar(children: [
+      ConferenceActionIconButton(
+          onPressedIcon: () => Navigator.of(context).pop(),
+          iconWidget: const Icon(Icons.phone),
+          backgroundIconColor: Colors.red)
+    ]);
   }
 }
