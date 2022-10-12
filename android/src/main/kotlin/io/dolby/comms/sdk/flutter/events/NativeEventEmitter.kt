5b9be330d4dd6787736d99ed83503fc11afe6ce9
kdrob <109142613+kdrob-dolby@users.noreply.github.com>
2022-10-12 12:07:16 +0200

    Switch to main thread for event delivery on android (#133)

--- android/src/main/kotlin/io/dolby/comms/sdk/flutter/events/NativeEventEmitter.kt
+++ android/src/main/kotlin/io/dolby/comms/sdk/flutter/events/NativeEventEmitter.kt
@@ -1,21 +1,26 @@
 package io.dolby.comms.sdk.flutter.events
 
+import android.os.Handler
+import android.os.Looper
 import com.voxeet.VoxeetSDK
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 
-abstract class NativeEventEmitter(private val handler: EventChannelHandler)  {
+abstract class NativeEventEmitter(private val eventChannel: EventChannelHandler) {
+
+    private val mainHandler: Handler = Handler(Looper.getMainLooper())
 
     fun onAttached(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-        handler.onAttached(flutterPluginBinding)
+        eventChannel.onAttached(flutterPluginBinding)
         VoxeetSDK.instance().register(this)
     }
 
     fun onDetached() {
         VoxeetSDK.instance().unregister(this)
-        handler.onDetached()
+        eventChannel.onDetached()
     }
 
-    fun emit(key: String, data: Any?) {
-        handler.emitEvent(key, data)
+    // Event needs to be emitted on main thread
+    fun emit(key: String, data: Any?) = mainHandler.post {
+        eventChannel.emitEvent(key, data)
     }
 }
