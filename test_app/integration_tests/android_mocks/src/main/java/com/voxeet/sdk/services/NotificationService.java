89d9a3e71a4dccb2dd69337d14d2cfb4bedc9c9b
mpozy <100850233+mpozy@users.noreply.github.com>
2022-10-13 13:42:28 +0200

    Add NotificationServiceAsserts (#137)

--- test_app/integration_tests/android_mocks/src/main/java/com/voxeet/sdk/services/NotificationService.java
+++ test_app/integration_tests/android_mocks/src/main/java/com/voxeet/sdk/services/NotificationService.java
@@ -8,18 +8,57 @@ import com.voxeet.sdk.json.ParticipantInvited;
 import com.voxeet.sdk.models.Conference;
 import com.voxeet.sdk.models.Participant;
 
+import org.jetbrains.annotations.Nullable;
+
 import java.util.List;
 
 public class NotificationService {
+
+    public static class InviteArgs {
+        @Nullable
+        private Conference conference;
+        @Nullable
+        private List<ParticipantInvited> participantsInvited;
+
+        public void setInviteArgs(Conference conference, List<ParticipantInvited> participantsInvited) {
+            this.conference = conference;
+            this.participantsInvited = participantsInvited;
+        }
+
+        @androidx.annotation.Nullable
+        public Conference getConference() {
+            return conference;
+        }
+
+        @androidx.annotation.Nullable
+        public List<ParticipantInvited> getParticipantsInvited() {
+            return participantsInvited;
+        }
+    }
+
+    private InviteArgs inviteArgs = new InviteArgs();
+    public InviteArgs getInviteArgs() {
+        return inviteArgs;
+    }
+
+    public boolean inviteHasRun = false;
     @NonNull
     public Promise<List<Participant>> inviteWithPermissions(@NonNull final Conference conference, @NonNull final List<ParticipantInvited> participantsInvited) {
+        inviteHasRun = true;
+        inviteArgs.setInviteArgs(conference, participantsInvited);
         return VoxeetSDK.conference().inviteWithPermissions(conference.getId(), participantsInvited);
     }
+
+    @Nullable
+    public Conference declineArgs;
+
+    public boolean declineHasRun = false;
 
     @NonNull
     public Promise<Boolean> decline(@NonNull final Conference conference) {
         String conferenceId = conference.getId();
-
+        declineHasRun = true;
+        declineArgs = conference;
         return Promise.resolve(true);
     }
 }
