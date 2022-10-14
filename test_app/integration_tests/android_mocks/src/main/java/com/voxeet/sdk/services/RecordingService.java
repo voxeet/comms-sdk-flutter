e1793c68d8989bc2a9844e94ea90186284ae22dd
mpozy <100850233+mpozy@users.noreply.github.com>
2022-10-14 15:35:43 +0200

    Add RecordingServiceAsserts (#144)

--- test_app/integration_tests/android_mocks/src/main/java/com/voxeet/sdk/services/RecordingService.java
+++ test_app/integration_tests/android_mocks/src/main/java/com/voxeet/sdk/services/RecordingService.java
@@ -6,7 +6,10 @@ import com.voxeet.promise.Promise;
 import org.jetbrains.annotations.NotNull;
 
 public class RecordingService {
+
+    public boolean startHasRun = false;
     public Promise<Boolean> start() {
+        startHasRun = true;
         return Promise.resolve(true);
     }
 
@@ -15,7 +18,9 @@ public class RecordingService {
      *
      * @return the promise to resolve that indicates the result of the request.
      */
+    public boolean stopHasRun = false;
     public Promise<Boolean> stop() {
+        stopHasRun = true;
         return Promise.resolve(true);
     }
 }
