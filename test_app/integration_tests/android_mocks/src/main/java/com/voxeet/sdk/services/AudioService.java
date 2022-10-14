adbfdcbca0fda345617159411760c170daaf6aec
Krzysztof Betlej <krzysztof.betlej@dolby.com>
2022-10-14 11:15:49 +0200

    Fix media device test after rebase to develop

---
+++ test_app/integration_tests/android_mocks/src/main/java/com/voxeet/sdk/services/AudioService.java
@@ -0,0 +1,12 @@
+package com.voxeet.sdk.services;
+
+import com.voxeet.promise.Promise;
+
+public class AudioService {
+
+    public boolean isSpeakerOn() {
+        return false;
+    }
+
+    public void setSpeakerMode(boolean isSpeakerOn) {}
+}
