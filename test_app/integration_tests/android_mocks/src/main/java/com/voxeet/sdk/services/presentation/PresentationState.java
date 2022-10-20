f60943354d963600ac5043d1c387eb0f23dcd43c
mpozy <100850233+mpozy@users.noreply.github.com>
2022-10-20 14:31:56 +0200

    Add VideoPresentationServiceAsserts (#149)

--- test_app/integration_tests/android_mocks/src/main/java/com/voxeet/sdk/services/presentation/PresentationState.java
+++ test_app/integration_tests/android_mocks/src/main/java/com/voxeet/sdk/services/presentation/PresentationState.java
@@ -1,10 +1,20 @@
 package com.voxeet.sdk.services.presentation;
 
 public enum PresentationState {
-    STARTED,
-    PLAY,
-    PAUSED,
-    SEEK,
-    STOP,
-    CONVERTED;
+    STOP(0),
+    PLAY(1),
+    PAUSED(2),
+    STARTED(3),
+    SEEK(4),
+    CONVERTED(5);
+
+    private final int value;
+
+    PresentationState(int value) {
+        this.value = value;
+    }
+
+    public int value() {
+        return value;
+    }
 }
