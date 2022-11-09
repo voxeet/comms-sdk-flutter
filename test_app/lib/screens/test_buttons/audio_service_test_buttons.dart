eb1c9616e0e57958dbb4a068656c912531e2ebf6
azduda <azduda@dolby.com>
2022-11-09 13:13:18 +0100

    Convert code to async/await in the audio service test buttons

--- test_app/lib/screens/test_buttons/audio_service_test_buttons.dart
+++ test_app/lib/screens/test_buttons/audio_service_test_buttons.dart
@@ -1,36 +1,40 @@
 import 'package:dolbyio_comms_sdk_flutter/dolbyio_comms_sdk_flutter.dart';
 import 'package:flutter/material.dart';
-
 import '/widgets/dialogs.dart';
 import '/widgets/secondary_button.dart';
 
-class AudioServiceTestButtons extends StatelessWidget {
-  final _dolbyioCommsSdkFlutterPlugin = DolbyioCommsSdk.instance;
+class AudioServiceTestButtons extends StatefulWidget {
+  const AudioServiceTestButtons({Key? key}) : super(key: key);
 
-  AudioServiceTestButtons({Key? key}) : super(key: key);
+  @override
+  State<AudioServiceTestButtons> createState() =>
+      _AudioServiceTestButtonsState();
+}
+
+class _AudioServiceTestButtonsState extends State<AudioServiceTestButtons> {
+  final _dolbyioCommsSdkFlutterPlugin = DolbyioCommsSdk.instance;
 
   @override
   Widget build(BuildContext context) {
-    var buttons = <Widget>[
-      SecondaryButton(
-          text: 'Get comfort noise level',
-          onPressed: () => getComfortNoiseLevel(context)),
-      SecondaryButton(
-          text: 'Set comfort noise level',
-          onPressed: () => setComfortNoiseLevel(context)),
-      SecondaryButton(
-          text: 'Start local audio', onPressed: () => startLocalAudio(context)),
-      SecondaryButton(
-          text: 'Stop local audio', onPressed: () => stopLocalAudio(context)),
-      SecondaryButton(
-          text: 'Get capture mode', onPressed: () => getCaptureMode(context)),
-      SecondaryButton(
-          text: 'Set capture mode', onPressed: () => setCaptureMode(context))
-    ];
     return Wrap(
       spacing: 8.0,
       runSpacing: 4.0,
-      children: buttons,
+      children: <Widget>[
+        SecondaryButton(
+            text: 'Get comfort noise level',
+            onPressed: () => getComfortNoiseLevel()),
+        SecondaryButton(
+            text: 'Set comfort noise level',
+            onPressed: () => setComfortNoiseLevel()),
+        SecondaryButton(
+            text: 'Start local audio', onPressed: () => startLocalAudio()),
+        SecondaryButton(
+            text: 'Stop local audio', onPressed: () => stopLocalAudio()),
+        SecondaryButton(
+            text: 'Get capture mode', onPressed: () => getCaptureMode()),
+        SecondaryButton(
+            text: 'Set capture mode', onPressed: () => setCaptureMode())
+      ],
     );
   }
 
@@ -43,58 +47,78 @@ class AudioServiceTestButtons extends StatelessWidget {
     );
   }
 
-  void getComfortNoiseLevel(BuildContext context) {
-    _dolbyioCommsSdkFlutterPlugin.audioService.localAudio
-        .getComfortNoiseLevel()
-        .then((comfortNoiseLevel) =>
-            showDialog(context, "Success", comfortNoiseLevel.toString()))
-        .onError((error, stackTrace) =>
-            showDialog(context, "Error", error.toString()));
+  Future<void> getComfortNoiseLevel() async {
+    try {
+      var comfortNoiseLevel = await _dolbyioCommsSdkFlutterPlugin
+          .audioService.localAudio
+          .getComfortNoiseLevel();
+      if (!mounted) return;
+      showDialog(context, 'Success', comfortNoiseLevel.toString());
+    } catch (error) {
+      if (!mounted) return;
+      showDialog(context, 'Error', error.toString());
+    }
   }
 
-  void setComfortNoiseLevel(BuildContext context) {
-    _dolbyioCommsSdkFlutterPlugin.audioService.localAudio
-        .setComfortNoiseLevel(ComfortNoiseLevel.medium)
-        .then((value) => showDialog(context, "Success", "OK"))
-        .onError((error, stackTrace) =>
-            showDialog(context, "Error", error.toString()));
+  Future<void> setComfortNoiseLevel() async {
+    try {
+      await _dolbyioCommsSdkFlutterPlugin.audioService.localAudio
+          .setComfortNoiseLevel(ComfortNoiseLevel.medium);
+      if (!mounted) return;
+      showDialog(context, 'Success', 'OK');
+    } catch (error) {
+      if (!mounted) return;
+      showDialog(context, 'Error', error.toString());
+    }
   }
 
-  void startLocalAudio(BuildContext context) {
-    _dolbyioCommsSdkFlutterPlugin.audioService.localAudio
-        .start()
-        .then((value) => showDialog(context, 'Success', 'OK'))
-        .onError(
-          (error, stackTrace) => showDialog(context, 'Error', error.toString()),
-        );
+  Future<void> startLocalAudio() async {
+    try {
+      await _dolbyioCommsSdkFlutterPlugin.audioService.localAudio.start();
+      if (!mounted) return;
+      showDialog(context, 'Success', 'OK');
+    } catch (error) {
+      if (!mounted) return;
+      showDialog(context, 'Error', error.toString());
+    }
   }
 
-  void stopLocalAudio(BuildContext context) {
-    _dolbyioCommsSdkFlutterPlugin.audioService.localAudio
-        .stop()
-        .then((value) => showDialog(context, 'Success', 'OK'))
-        .onError(
-          (error, stackTrace) => showDialog(context, 'Error', error.toString()),
-        );
+  Future<void> stopLocalAudio() async {
+    try {
+      await _dolbyioCommsSdkFlutterPlugin.audioService.localAudio.stop();
+      if (!mounted) return;
+      showDialog(context, 'Success', 'OK');
+    } catch (error) {
+      if (!mounted) return;
+      showDialog(context, 'Error', error.toString());
+    }
   }
 
-  void getCaptureMode(BuildContext context) {
-    _dolbyioCommsSdkFlutterPlugin.audioService.localAudio
-        .getCaptureMode()
-        .then((audioCaptureOptions) => showDialog(
-            context, 'Success', audioCaptureOptions.toJson().toString()))
-        .onError((error, stackTrace) =>
-            showDialog(context, 'Error', error.toString()));
+  Future<void> getCaptureMode() async {
+    try {
+      final audioCaptureOptions = await _dolbyioCommsSdkFlutterPlugin
+          .audioService.localAudio
+          .getCaptureMode();
+      if (!mounted) return;
+      showDialog(context, 'Success', audioCaptureOptions.toJson().toString());
+    } catch (error) {
+      if (!mounted) return;
+      showDialog(context, 'Error', error.toString());
+    }
   }
 
-  void setCaptureMode(BuildContext context) {
-    var mode = AudioCaptureMode.standard;
-    var noiseReduction = NoiseReduction.high;
-    var options = AudioCaptureOptions(mode, noiseReduction);
-    _dolbyioCommsSdkFlutterPlugin.audioService.localAudio
-        .setCaptureMode(options)
-        .then((value) => showDialog(context, 'Success', 'OK'))
-        .onError((error, stackTrace) =>
-            showDialog(context, 'Error', error.toString()));
+  Future<void> setCaptureMode() async {
+    try {
+      var mode = AudioCaptureMode.standard;
+      var noiseReduction = NoiseReduction.low;
+      var options = AudioCaptureOptions(mode, noiseReduction);
+      await _dolbyioCommsSdkFlutterPlugin.audioService.localAudio
+          .setCaptureMode(options);
+      if (!mounted) return;
+      showDialog(context, 'Success', 'OK');
+    } catch (error) {
+      if (!mounted) return;
+      showDialog(context, 'Error', error.toString());
+    }
   }
 }
