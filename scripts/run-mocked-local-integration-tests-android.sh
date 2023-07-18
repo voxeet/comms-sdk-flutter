fc02b3b0e7e10f8cd3e8ae0a483a344343ed102b
kbetl-dlb <krzysztof.betlej@dolby.com>
2023-07-18 12:50:37 +0200

    Fix script to run android mocked test localy

--- scripts/run-mocked-local-integration-tests-android.sh
+++ scripts/run-mocked-local-integration-tests-android.sh
@@ -1,51 +1,16 @@
 #! /usr/bin/env bash
 
-device_name="testAVD"
-system_image="system-images;android-29;default;x86"
-device_port="5554"
-serial_no="emulator-$device_port"
-gpu_mode="swiftshader_indirect"
-
-
 export ANDROID_HOME=~/.android/
-export HOME=/Users/runner
+export HOME=~/
 
 export PATH="$PATH:$ANDROID_SDK_ROOT/emulator:$ANDROID_SDK_ROOT/platform-tools:$ANDROID_SDK_ROOT/cmdline-tools/latest/bin"
 
-echo $PATH
-
-echo "Check available avd"
-
-avdmanager list avd
-
-echo "Create system image: $system_image"
-
-echo "no" | avdmanager --verbose create avd --force --name $device_name --abi "default/x86" --package "$system_image"
-echo "disk.dataPartition.size=8G" >> ~/.android/avd/$device_name.avd/config.ini
-echo "hw.ramSize = 3.072MB" >> ~/.android/avd/$device_name.avd/config.ini
-touch ~/.android/emu-update-last-check.ini
-
-ls -la ~/.android/
-
 adb start-server
 
-sleep 5
-
-echo "accel check: "
-emulator -accel-check
-
-echo "check emulator list:"
-emulator -list-avds
-
-echo "start: $device_name"
-$ANDROID_SDK_ROOT/emulator/emulator -avd $device_name -port $device_port -noaudio -no-window -no-snapshot -no-boot-anim &
-
-echo "Device name: $device_name"
-
-sleep 8
+sleep 1
 
 i=1
-while [ "`adb -s $serial_no shell getprop sys.boot_completed | tr -d '\r' `" != "1" ] ; do
+while [ "`adb shell getprop sys.boot_completed | tr -d '\r' `" != "1" ] ; do
   sleep 2;
   i=$((i+1))
   if [[ "$i" -gt 120 ]]; then
@@ -55,17 +20,13 @@ while [ "`adb -s $serial_no shell getprop sys.boot_completed | tr -d '\r' `" != 
 
 done
 
-echo "Device: $device_name is booted"
-
-adb devices
-
 ./scripts/change-to-mock.sh
 
 currentFolder=`pwd`
 
 cd test_app
 
-USE_SDK_MOCK=true flutter test integration_tests/mocked/master_test.dart -d $serial_no
+USE_SDK_MOCK=true flutter test integration_tests/mocked/master_test.dart
 test_exit_code=$?
 
 cd $currentFolder
@@ -73,10 +34,5 @@ cd $currentFolder
 ./scripts/remove-mocks.sh
 
 echo "Shutting down simulator $device_id ..."
-adb -s $serial_no emu kill
-echo "Simulator $device_name shut down"
-wait
-echo "Remove avd: "
-avdmanager delete avd -n testAVD
 
 exit $test_exit_code
